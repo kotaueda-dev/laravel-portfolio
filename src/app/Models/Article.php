59c4e459012200c3d4b1601a1a0cf9b17e256886
kota.ueda <kota.ueda@keenova.co.jp>
2025-12-12 12:17:57 +0900

    feat: ArticleモデルとUserモデルにリレーションを追加

--- src/app/Models/Article.php
+++ src/app/Models/Article.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Article extends Model
@@ -15,6 +16,11 @@ class Article extends Model
         'content',
         'user_id',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 
     public function comments(): HasMany
     {
