49c3a31d18fb7e05ac3b9836cce29a990617577a
kota.ueda <kota.ueda@keenova.co.jp>
2025-12-12 16:42:45 +0900

    refactor: 記事のルーティングを整理

--- src/routes/api.php
+++ src/routes/api.php
@@ -7,30 +7,25 @@ use Illuminate\Support\Facades\Route;
 
 // サインアップ・退会
 Route::post('/signup', [AuthController::class, 'register']);
-Route::delete('/user', [AuthController::class, 'deleteAccount'])
-    ->middleware('auth:sanctum');
+Route::delete('/user', [AuthController::class, 'deleteAccount'])->middleware('auth:sanctum');
 
 // ログイン・ログアウト
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])
-    ->middleware('auth:sanctum');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 // 記事
-Route::get('/articles', [ArticleController::class, 'index']);
-Route::get('/articles/{id}', [ArticleController::class, 'show'])
-    ->where('id', '[0-9]+');
-Route::post('/articles/{id}/likes', [ArticleController::class, 'like'])
-    ->where('id', '[0-9]+');
+Route::prefix('articles')->group(function () {
+    Route::get('/', [ArticleController::class, 'index']);
+    Route::get('/{id}', [ArticleController::class, 'show'])->where('id', '[0-9]+');
+    Route::post('/{id}/likes', [ArticleController::class, 'like'])->where('id', '[0-9]+');
 
-// 認証済みユーザーのみ
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/articles', [ArticleController::class, 'store']);
-    Route::put('/articles/{id}', [ArticleController::class, 'update'])
-        ->where('id', '[0-9]+');
-    Route::delete('/articles/{id}', [ArticleController::class, 'destroy'])
-        ->where('id', '[0-9]+');
+    // 認証済みユーザーのみ
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/', [ArticleController::class, 'store']);
+        Route::put('/{id}', [ArticleController::class, 'update'])->where('id', '[0-9]+');
+        Route::delete('/{id}', [ArticleController::class, 'destroy'])->where('id', '[0-9]+');
+    });
 });
 
 // // コメント
-Route::post('/articles/{article}/comments', [CommentController::class, 'store'])
-    ->where('id', '[0-9]+');
+Route::post('/articles/{article}/comments', [CommentController::class, 'store'])->where('id', '[0-9]+');
