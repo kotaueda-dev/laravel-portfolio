e914449aff6a68ba82672fe0db8f3454cf91fa41
kota.ueda <kota.ueda@keenova.co.jp>
2026-01-08 18:06:57 +0900

    feat: 不要なThrowableのインポートを削除

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -17,7 +17,6 @@ use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException;
 use Symfony\Component\HttpKernel\Exception\UnsupportedMediaTypeHttpException;
-use Throwable;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
